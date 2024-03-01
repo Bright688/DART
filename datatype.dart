cb25daa5b0025e9190a79323b1df07b4dad955fc
Bright Akaraiwe <32075232-chidoziebright0@users.noreply.replit.com>
2024-03-01 17:58:31 +0000

    Initial commit

---
+++ datatype.dart
@@ -0,0 +1,21 @@
+void main() {
+  //int data type
+  int age = 50;
+  print('John age is ${age}');
+
+  //double data type
+  double weight=50.5;
+  print('John weight is ${weight}');
+
+  //string data type
+  String profession="doctor";
+  print('john is a ${profession}');
+
+  //List data type
+  List hobbies=["reading", "listening to music", "skipping"];
+print('John hobbies are ${hobbies}');
+
+  //map data type
+  Map lang={"language1": "English", "language2": "French"};
+  print('John languages are ${lang}');
+}
